2d1e8a68db0b894dba2567deccb4f8d06fe5cee2
Tanya584 <25110cn192@glbitm.ac.in>
2026-02-23 14:26:57 +0530

    Update person_age.c

--- person_age.c
+++ person_age.c
@@ -1,40 +1,23 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-    int age, category;
+    int age;
 
     printf("Enter age: ");
     scanf("%d", &age);
 
-    // Assign category based on age
     if (age >= 0 && age <= 12)
-        category = 1;   // Child
-    else if (age <= 18)
-        category = 2;   // Teenager
-    else if (age <= 60)
-        category = 3;   // Adult
-    else if (age >= 61)
-        category = 4;   // Senior Citizen
+           printf("Child\n");   // Child
+    else if (age <= 19)
+        printf("Teenager\n");   // Teenager
+    else if (age <= 59)
+         printf("Adult\n");   // Adult
+    else if (age >= 60)
+         printf("Senior Citizen\n");  // Senior Citizen
     else
-        category = 0;   // Invalid
-
-    // Switch case
-    switch(category) {
-        case 1:
-            printf("Category: Child\n");
-            break;
-        case 2:
-            printf("Category: Teenager\n");
-            break;
-        case 3:
-            printf("Category: Adult\n");
-            break;
-        case 4:
-            printf("Category: Senior Citizen\n");
-            break;
-        default:
-            printf("Invalid age entered.\n");
-    }
+         printf("Invalid age entered.\n");   // Invalid
 
     return 0;
 }
+  
